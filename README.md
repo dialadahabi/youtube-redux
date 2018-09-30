dd0ee47bd3422e58a1c3d9b571936bec2252a5be
dialadahabi <dahabidiala@hotmail.com>
2018-09-30 21:18:29 +0300

    create components files

---
+++ README.md
@@ -0,0 +1,14 @@
+# youtube-redux
+# Boilerplate:
+https://github.com/StephenGrider/ReduxSimpleStarter
+
+# Development:
+
+- Redux
+- Redux-thunk
+- React
+- YoutubeAPI-Search
+
+# Testing:
+
+- Mocha
---
+++ src/index.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import searchBar from './components/search_bar';
+
+const API_KEY = 'AIzaSyBrztENyAU478aebvL6mYcaIrxrX-2_k2c';
+
+const App = () => {
+	return <div>Hi Diala!</div>;
+}
+
+ReactDOM.render(<App />, document.querySelector('.container'));
+
